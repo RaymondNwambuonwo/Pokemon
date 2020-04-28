9708a1e49b9b6f8785c6625e416c577cad1c5edb
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-28 16:30:44 -0400

    application skeleton setup

---
+++ pokemon/src/Pagination.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function Pagination() {
+  return <div>heloow</div>;
+}
+
+export default Pagination;
---
+++ pokemon/src/Pokelist.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function Pokelist() {
+  return <div>heloow</div>;
+}
+
+export default Pokelist;
