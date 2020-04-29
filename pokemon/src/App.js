0009f14c26209ad3b4f4e3d307ba7f128bb96c9f
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-28 20:20:47 -0400

    was able to render data from api with hooks

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -5,12 +5,19 @@ import "./App.css";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+    axios.get(currentPageUrl).then((res) => {
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.previous);
       setPokemon(res.data.results.map((p) => p.name));
     });
-  }, []);
+  }, [currentPageUrl]);
 
   return <Pokelist pokemon={pokemon} />;
 }
