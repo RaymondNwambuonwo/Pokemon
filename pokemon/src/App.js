6d925e0c74c13ad7e85c44d7b6d87a7e696b92f9
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-28 20:02:26 -0400

    successfully implemented axios and hooks to get data

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,13 +1,18 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Pokelist from "./Pokelist";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+  const [pokemon, setPokemon] = useState([]);
 
-  return (
-    <Pokelist pokemon={pokemon}/>
-  )
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      setPokemon(res.data.results.map((p) => p.name));
+    });
+  }, []);
+
+  return <Pokelist pokemon={pokemon} />;
 }
 
 export default App;
