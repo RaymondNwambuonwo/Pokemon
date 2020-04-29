84961cf9049fd7b351178b01ba830c36931c1d70
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-28 20:49:13 -0400

    setup previous and next buttons with user able to see 20 pokemon at a time

--- pokemon/src/Pagination.js
+++ pokemon/src/Pagination.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 
-function Pagination() {
-  return <div>heloow</div>;
+function Pagination({ gotoNextPage, gotoPrevPage }) {
+  return (
+    <div>
+      {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
+      {gotoPrevPage && <button onClick={gotoNextPage}>Next</button>}
+    </div>
+  );
 }
 
 export default Pagination;
